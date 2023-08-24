4c837e0b3e39ec8beae75eb4457cfc4baa1ca7fa
Harmeet Singh <evildoremon@gmail.com>
2023-08-24 23:44:44 +0530

    New about page!
    Since creating blog is not for visitors, an About page is added instead and the create page is only shown to logged user!

--- src/components/pages/BlogsComponents/RenderBlogs.jsx
+++ src/components/pages/BlogsComponents/RenderBlogs.jsx
@@ -1,23 +1,35 @@
-import { Grow, Typography, Container, Stack } from "@mui/material";
+import {
+    Grow,
+    Typography,
+    Container,
+    Stack,
+    Avatar,
+    CircularProgress,
+    Skeleton,
+} from "@mui/material";
 import {
     BlogButton,
     BlogsImageBox,
     navigate,
 } from "../../shared/CustomComponents";
+import { useState } from "react";
 
 export default function RenderBlogs(props) {
-    return Object.keys(props.blogs).map((key, index) => {
-        const { _id, title, image, author, date } = props.blogs[key];
+    return Object.keys(props.blogs).map((key) => {
+        const { _id, title, image, author, date, avatar } = props.blogs[key];
+
+        const [isImageLoading, setIsImageLoading] = useState(true);
+        const [isAvatarLoading, setIsAvatarLoading] = useState(true);
 
         return (
             <Grow
                 in={true}
-                key={_id}
                 style={{
-                    transitionDelay: index * 100,
+                    transitionDelay: key * 100,
                 }}
             >
                 <BlogButton
+                    key={_id}
                     onClick={() => {
                         setTimeout(() => {
                             navigate(`/blog/${_id}`);
@@ -26,29 +38,64 @@ export default function RenderBlogs(props) {
                         props.setShowLoading(true);
                     }}
                 >
-                    <BlogsImageBox component="img" src={image} alt={title} />
+                    {isImageLoading && (
+                        <CircularProgress color="icon" size={30} />
+                    )}
+
+                    <BlogsImageBox
+                        component="img"
+                        src={image}
+                        alt={title}
+                        onError={() => setIsImageLoading(true)}
+                        onLoad={() => {
+                            setIsImageLoading(false);
+                        }}
+                        sx={{
+                            display: isImageLoading && "none",
+                        }}
+                    />
 
                     <Stack spacing={2} component={Container}>
                         <Typography
-                            sx={{ textAlign: "left", cursor: "pointer" }}
+                            sx={{
+                                textAlign: "left",
+                                cursor: "pointer",
+                            }}
                             fontWeight="600"
                             variant="h4"
                         >
                             {title}
                         </Typography>
+                        <Stack spacing={2} direction="row">
+                            {isAvatarLoading && (
+                                <Skeleton variant="circular">
+                                    <Avatar />
+                                </Skeleton>
+                            )}
 
-                        <Stack
-                            direction="column"
-                            alignItems="flex-start"
-                            justifyContent="flex-start"
-                            sx={{
-                                opacity: 0.8,
-                            }}
-                        >
-                            <Typography variant="body2">
-                                By <strong>{author}</strong>
-                            </Typography>
-                            <Typography variant="body2">{date}</Typography>
+                            <Grow in={true}>
+                                <Avatar
+                                    src={avatar && avatar}
+                                    onLoad={() => setIsAvatarLoading(false)}
+                                    sx={{
+                                        display: isAvatarLoading && "none",
+                                    }}
+                                />
+                            </Grow>
+                            <Stack
+                                direction="column"
+                                alignItems="flex-start"
+                                justifyContent="flex-start"
+                                sx={{
+                                    color: (theme) =>
+                                        theme.palette.text.disabled,
+                                }}
+                            >
+                                <Typography variant="body2">
+                                    By <strong>{author}</strong>
+                                </Typography>
+                                <Typography variant="body2">{date}</Typography>
+                            </Stack>
                         </Stack>
                     </Stack>
                 </BlogButton>
