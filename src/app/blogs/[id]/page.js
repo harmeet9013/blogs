c6ad91728898059ebdc675fd2f1711deee6fced2
harmeet9013 <evildoremon@gmail.com>
2024-11-10 13:35:38 +0530

    build issue fix

--- src/app/blogs/[id]/page.js
+++ src/app/blogs/[id]/page.js
@@ -2,7 +2,7 @@ import { BlogView } from "@/sections";
 import { endpoints } from "@/lib/axios";
 import { GET_REQUEST } from "@/services";
 
-export default async function ({ params }) {
+export default async function BlogPage({ params }) {
     const { id } = params;
 
     const response = await GET_REQUEST(endpoints["blogs"], { blog_id: id });
